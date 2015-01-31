bc1997e8261c055d5673dd9ccadf3a3bdb0e1309
rbaselio <rbaselio@gmail.com>
2015-01-30 22:02:25 -0200

    Upload Inicial

---
+++ PriceTag/app/src/main/java/com/preco/milionarios/pricetag/ParseHtml.java
@@ -0,0 +1,32 @@
+package com.preco.milionarios.pricetag;
+
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+public class ParseHtml {
+
+    public static String getString(String site) {
+
+        Document doc;
+        try {
+
+            // need http protocol
+            doc = Jsoup.connect(site).get();
+
+            // get page title
+            Elements descr = doc.getElementsByTag("h1");
+            for (Element d : descr) return descr.text().substring(0, descr.text().indexOf("GTIN"));
+            return "num deu";
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
+}
---
+++ PriceTag/app/src/main/java/com/preco/milionarios/pricetag/PriceTag.java
@@ -0,0 +1,74 @@
+package com.preco.milionarios.pricetag;
+
+import android.content.Intent;
+import android.os.StrictMode;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class PriceTag extends ActionBarActivity {
+
+    private Button getLeitura;
+    private TextView resultado;
+    private String contents;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_price_tag);
+        if (android.os.Build.VERSION.SDK_INT > 9) {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
+
+        getLeitura = (Button) findViewById(R.id.getLeitura);
+        resultado = (TextView) findViewById(R.id.resultado);
+
+
+        getLeitura.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+                intent.putExtra("SCAN_MODE", "PRODUCT_MODE");
+                startActivityForResult(intent, 0);
+            }
+        });
+
+        if (savedInstanceState != null) {
+            resultado.setText(contents);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_price_tag, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        if (requestCode == 0) {
+            if (resultCode == RESULT_OK) {
+                contents = intent.getStringExtra("SCAN_RESULT");
+                resultado.setText(ParseHtml.getString("http://cosmos.bluesoft.com.br/products/" + contents));
+            } else if (resultCode == RESULT_CANCELED) {
+                // Handle cancel
+            }
+        }
+    }
+}
