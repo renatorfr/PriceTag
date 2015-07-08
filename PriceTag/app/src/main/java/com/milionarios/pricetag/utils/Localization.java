4c1cff08e362c68c4aa5142b5f01ff6537cdea79
Roberto <rbaselio@gmail.com>
2015-07-07 21:44:04 -0300

    implements TODO for gps errors

--- PriceTag/app/src/main/java/com/milionarios/pricetag/utils/Localization.java
+++ PriceTag/app/src/main/java/com/milionarios/pricetag/utils/Localization.java
@@ -1,6 +1,9 @@
 package com.milionarios.pricetag.utils;
 
+
+import android.app.Activity;
 import android.content.Context;
+import android.content.IntentSender;
 import android.location.Location;
 import android.os.Bundle;
 import android.widget.Toast;
@@ -10,6 +13,8 @@ import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
+
+
 import com.milionarios.pricetag.utils.interfaces.LocationObserver;
 
 import java.util.ArrayList;
@@ -17,12 +22,14 @@ import java.util.List;
 
 public class Localization implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
     private static Localization instance = null;
+    private final static int  CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
     private Context context;
     private Location mLastLocation;
     private Boolean mRequestingLocationUpdates;
     private LocationRequest mLocationRequest;
     private GoogleApiClient mGoogleApiClient;
     private List<LocationObserver> observers = new ArrayList<LocationObserver>();
+
 
     private Localization() {
 
@@ -87,13 +94,35 @@ public class Localization implements GoogleApiClient.ConnectionCallbacks, Google
 
     @Override
     public void onConnectionSuspended(int i) {
-        Toast.makeText(context, "GPS Suspenso", Toast.LENGTH_LONG);
+        Toast.makeText(context, "GPS incapaz de determinar sua posição " + i, Toast.LENGTH_LONG);
+        mGoogleApiClient.connect();
     }
 
     @Override
     public void onConnectionFailed(ConnectionResult connectionResult) {
-        Toast.makeText(context, "GPS Falhou", Toast.LENGTH_LONG);
+          /*
+             * Google Play services can resolve some errors it detects. If the error
+            * has a resolution, try sending an Intent to start a Google Play
+            * services activity that can resolve error.
+            */
+        if (connectionResult.hasResolution()) {
+            try {
+                // Start an Activity that tries to resolve the error
+                connectionResult.startResolutionForResult((Activity) context, CONNECTION_FAILURE_RESOLUTION_REQUEST);
+            /*
+             * Thrown if Google Play services canceled the original
+             * PendingIntent
+             */
+            } catch (IntentSender.SendIntentException e) {
+                // Log the error
+                e.printStackTrace();
+            }
+        } else {
+            Toast.makeText(context, "GPS incapaz de determinar sua posição", Toast.LENGTH_LONG);
+
+        }
     }
+
 
     @Override
     public void onLocationChanged(Location location) {
